eeacfb2f06aa33daa1cee8d9f7c45fed8d9c6dfe
SamiBSB <53528941+SamiBSB@users.noreply.github.com>
2020-05-03 08:19:30 +0200

    Feat: Entities With Timestamps

---
+++ src/Controller/ListController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ListController extends AbstractFOSRestController
+{
+
+    public function getListsAction()
+    {
+
+    }
+
+    public function postListsAction()
+    {
+
+    }
+
+    public function getListsTasksAction(int $id)
+    {
+
+    }
+}
